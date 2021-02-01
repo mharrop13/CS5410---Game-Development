c86c2265cfaae63738714b4918e660c2b4fc2662
Michael Harrop <michael.harrop13@gmail.com>
2021-02-01 15:38:59 -0700

    Moved all HTML elements into index.html to simplify gameloop.js

--- Assn1/gameloop.js
+++ Assn1/gameloop.js
@@ -1,92 +1,6 @@
 document.title = "CS5410 HW1 - Browser GameLoop";
 const BODY = document.body;
 
-
-//Initialize Game - Load Graphics lines 6 - 88
-const HEADER = document.createElement('div');
-HEADER.setAttribute('class', 'header bold blue');
-BODY.appendChild(HEADER);
-
-let title = document.createElement('h1');
-title.textContent = "Browser GameLoop";
-HEADER.appendChild(title);
-
-let content = document.createElement('div');
-content.setAttribute('class', 'white');
-BODY.appendChild(content);
-
-let consoleBox = document.createElement('div');
-consoleBox.setAttribute('id', 'console')
-consoleBox.setAttribute('class', 'consoleBox white');
-content.appendChild(consoleBox);
-
-let inputBox = document.createElement('div');
-inputBox.setAttribute('class', 'inputBox  white');
-content.appendChild(inputBox);
-
-let footer = document.createElement('div');
-footer.setAttribute('class', 'header blue');
-BODY.appendChild(footer);
-
-let signature = document.createElement('h3');
-signature.textContent = "Created by Michael Harrop for CS5410";
-footer.appendChild(signature);
-
-let inputForm = document.createElement('form');
-inputForm.setAttribute('id', 'myform');
-inputBox.appendChild(inputForm);
-
-let nameLabel = document.createElement('label')
-nameLabel.setAttribute('for', "nameInput");
-nameLabel.textContent = "Name: ";
-inputForm.appendChild(nameLabel);
-
-let nameInput = document.createElement('input');
-nameInput.setAttribute('type', 'text');
-nameInput.setAttribute('id', "nameInput");
-nameInput.setAttribute('name', "nameInput");
-nameInput.setAttribute('value', 'First');
-inputForm.appendChild(nameInput);
-
-let linebreak = document.createElement('br');
-inputForm.appendChild(linebreak);
-
-let intervalLabel = document.createElement('label')
-intervalLabel.setAttribute('for', "intervalInput");
-intervalLabel.textContent = "Interval: ";
-inputForm.appendChild(intervalLabel);
-
-let intervalInput = document.createElement('input');
-intervalInput.setAttribute('type', 'number');
-intervalInput.setAttribute('id', "intervalInput");
-intervalInput.setAttribute('name', "intervalInput");
-intervalInput.setAttribute('value', '1');
-inputForm.appendChild(intervalInput);
-
-let lineBreak2 = document.createElement('br');
-inputForm.appendChild(lineBreak2);
-
-let occurenceLabel = document.createElement('label')
-occurenceLabel.setAttribute('for', "occurenceInput");
-occurenceLabel.textContent = "# Times: ";
-inputForm.appendChild(occurenceLabel);
-
-let occurenceInput = document.createElement('input');
-occurenceInput.setAttribute('type', 'number');
-occurenceInput.setAttribute('id', "occurenceInput");
-occurenceInput.setAttribute('name', "occurenceInput");
-occurenceInput.setAttribute('value', '10');
-inputForm.appendChild(occurenceInput);
-
-let lineBreak3 = document.createElement('br');
-inputForm.appendChild(lineBreak3);
-
-let addEventButton = document.createElement('input');
-addEventButton.setAttribute('type', 'button');
-addEventButton.setAttribute('onclick', 'addEventToQueue()');
-addEventButton.setAttribute('value', 'Compute');
-inputForm.appendChild(addEventButton);
-
 //Timed Event Class provides an easy structure for events to comply to when
 //button is pressed. Receives values from nameInput, intervalInput, and occurenceInput
 class TimedEvent {
@@ -100,7 +14,7 @@ class TimedEvent {
 
 //When button is clicked, this method makes an event and adds it to the
 //array of events
-function addEventToQueue() {
+function processInput() {
     let makeEvent = new TimedEvent(nameInput.value, intervalInput.value, occurenceInput.value);
     eventArray.push(makeEvent);
 
